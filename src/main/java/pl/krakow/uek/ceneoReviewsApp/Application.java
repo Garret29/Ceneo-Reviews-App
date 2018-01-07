7d802cefd27f309a6b4fa54b5233b097d163ffb6
Garret29 <szymon.piotrowski93@gmail.com>
2018-01-07 13:40:48 +0100

    commit

--- src/main/java/pl/krakow/uek/ceneoReviewsApp/Application.java
+++ src/main/java/pl/krakow/uek/ceneoReviewsApp/Application.java
@@ -16,11 +16,4 @@ public class Application {
         SpringApplication.run(Application.class, args);
 
     }
-
-    @Bean
-    public CommandLineRunner commandLineRunner(ProductRepository productRepository) {
-        return args -> {
-
-        };
-    }
 }
